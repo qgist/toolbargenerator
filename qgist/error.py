5f4a8ec8a89eb26242b50f730898bb4b7d1639b4
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-19 18:07:49 +0200

    error

---
+++ qgist/error.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+QGIST TOOLBARGENERATOR
+QGIS Plugin for Generating Toolbars
+https://github.com/qgist/toolbargenerator
+
+    qgist/error.py: QGIST errors
+
+    Copyright (C) 2017-2019 QGIST project <info@qgist.org>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU General Public License
+Version 2 ("GPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+https://github.com/qgist/toolbargenerator/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# EXCEPTIONS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class QgistAttributeError(AttributeError):
+    pass
+
+class QgistConfigKeyError(KeyError):
+    pass
+
+class QgistTranslationError(Exception):
+    pass
+
+class QgistTypeError(TypeError):
+    pass
+
+class QgistValueError(ValueError):
+    pass
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ALL EXCEPTIONS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Qgist_ALL_Errors = tuple([v for k, v in globals().items() if k.startswith('Qgist')])
