1637443a3684e2f46c3d8df8420b5d3420181996
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-19 18:04:58 +0200

    core class

---
+++ qgist/toolbargenerator/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+QGIST TOOLBARGENERATOR
+QGIS Plugin for Generating Toolbars
+https://github.com/qgist/toolbargenerator
+
+    qgist/toolbargenerator/__init__.py: QGIST toolbargenerator root
+
+    Copyright (C) 2017-2019 QGIST project <info@qgist.org>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU General Public License
+Version 2 ("GPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+https://github.com/qgist/toolbargenerator/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT (Internal)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from .core import toolbargenerator
