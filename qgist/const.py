49216c3d1ecb200b8a1a8af67fc471a8e6a6b988
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-19 18:07:09 +0200

    const

---
+++ qgist/const.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+QGIST TOOLBARGENERATOR
+QGIS Plugin for Generating Toolbars
+https://github.com/qgist/toolbargenerator
+
+    qgist/const.py: QGIST constants
+
+    Copyright (C) 2017-2019 QGIST project <info@qgist.org>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU General Public License
+Version 2 ("GPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+https://github.com/qgist/toolbargenerator/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CONST
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+DEFAULT_FLD = 'defaults'
+ICON_FLD = 'icons'
+QGIS_CONFIG_FLD = 'QGIS'
+QGIST_CONFIG_FLD = 'qgist'
+TRANSLATION_FLD = 'i18n'
