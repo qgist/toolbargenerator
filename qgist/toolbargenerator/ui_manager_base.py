6d4806366cd22e8581285dfec206d261eac9d541
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-20 21:05:50 +0200

    added header

---
+++ qgist/toolbargenerator/ui_manager_base.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+QGIST TOOLBARGENERATOR
+QGIS Plugin for Generating Toolbars
+https://github.com/qgist/toolbargenerator
+
+    qgist/toolbargenerator/ui_manager_base.py: toolbargenerator manager ui base class
+
+    Copyright (C) 2017-2019 QGIST project <info@qgist.org>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU General Public License
+Version 2 ("GPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+https://github.com/qgist/toolbargenerator/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
